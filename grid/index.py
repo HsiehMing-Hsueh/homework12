1223a42ef8d5a995595228e98864de37e04983cf
謝明學 <gjerry40105@gmail.com>
2023-03-27 16:10:40 +0800

    modify

--- grid/index.py
+++ grid/index.py
@@ -36,24 +36,24 @@ class Window(tk.Tk):
 
         #姓名欄位
         ttk.Label(bottomFrame,text="姓名:",style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
-        nameEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
-        nameEntry.grid(column=1,row=0,sticky=tk.W,padx=10)
+        self.nameEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        self.nameEntry.grid(column=1,row=0,sticky=tk.W,padx=10)
 
         #出生年月日欄位
         ttk.Label(bottomFrame,text="出生年月日:",style='gridLabel.TLabel').grid(column=0,row=1,sticky=tk.E)
         ttk.Label(bottomFrame,text="(2000/03/01)",style='gridLabel.TLabel').grid(column=0,row=2,sticky=tk.E)
-        birthEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
-        birthEntry.grid(column=1, row=1, sticky=tk.W, rowspan=2, padx=10)
+        self.birthEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        self.birthEntry.grid(column=1, row=1, sticky=tk.W, rowspan=2, padx=10)
 
         #身高欄位
         ttk.Label(bottomFrame,text="身高(cm):",style='gridLabel.TLabel').grid(column=0,row=3,sticky=tk.E)
-        heightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
-        heightEntry.grid(column=1, row=3, sticky=tk.W, padx=10)
+        self.heightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        self.heightEntry.grid(column=1, row=3, sticky=tk.W, padx=10)
 
         #體重欄位
         ttk.Label(bottomFrame,text="體重(kg):",style='gridLabel.TLabel').grid(column=0,row=4,sticky=tk.E)
-        weightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
-        weightEntry.grid(column=1, row=4, sticky=tk.W, padx=10)
+        self.weightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
+        self.weightEntry.grid(column=1, row=4, sticky=tk.W, padx=10)
 
         #訊息顯示區域
         messageText = tk.Text(bottomFrame,height=5,width=35,state=tk.DISABLED)
@@ -63,6 +63,35 @@ class Window(tk.Tk):
         commitBtn = ttk.Button(bottomFrame,text="計算")
         commitBtn.grid(column=1,row=6,sticky=tk.W)
 
+    #BMI計算
+    def BMI(self,weight,height):
+        bmi = weight // (height/100) ** 2
+        if bmi < 18.5:
+            "太輕"
+        elif bmi < 24:
+            "正常"
+        elif bmi < 27:
+            "過重"
+        elif bmi < 30:
+            "輕度肥胖"
+        elif bmi < 35:
+            "中度肥胖"
+        else:
+            "重度肥胖"
+
+    def check_value(self,name,birth,height,weigrt):
+        if name.get() =='':
+            self.nameEntry.message('名字未輸入值')
+        
+    def click_btn(self):
+        pass
+
+    def print_message(self):
+        pass
+    #清除身高體重輸入的值
+    def clear_text(self):
+        self.heightEntry.delete(0,'end')
+        self.weightEntry.delete(0,'end')
 
 
 
